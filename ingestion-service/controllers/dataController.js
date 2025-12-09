bcc56fea9de43338913dd74b75c39a6e2b57e478
ligten000 <161305515+ligten000@users.noreply.github.com>
2025-12-09 20:35:03 +0700

    set rabbitURL

--- ingestion-service/controllers/dataController.js
+++ ingestion-service/controllers/dataController.js
@@ -2,12 +2,18 @@ const amqp = require('amqplib');
 
 let channel, connection;
 
-// khởi tạo RabbitMQ connection
+// Kết nối RabbitMQ
 async function connectQueue() {
     try {
-        connection = await amqp.connect('amqp://localhost');
+        const RABBIT_URL = process.env.RABBIT_URL;
+
+        console.log("Connecting to RabbitMQ:", RABBIT_URL);
+
+        connection = await amqp.connect(RABBIT_URL);
         channel = await connection.createChannel();
+
         await channel.assertQueue("sensor_data");
+
         console.log("Ingestion Service connected to RabbitMQ");
     } catch (error) {
         console.error("RabbitMQ connection error:", error);
@@ -16,18 +22,25 @@ async function connectQueue() {
 
 connectQueue();
 
+// API nhận dữ liệu
 exports.receiveData = async (req, res) => {
     const data = req.body;
 
     console.log("Data received:", data);
 
-    // publish vào queue
     try {
+        if (!channel) {
+            console.error("Channel not ready");
+            return res.status(500).json({ message: "Queue not ready" });
+        }
+
         channel.sendToQueue("sensor_data", Buffer.from(JSON.stringify(data)));
         console.log("Published to queue:", data);
+
+        res.json({ message: "Data received" });
+
     } catch (err) {
         console.error("Queue publish error:", err);
+        res.status(500).json({ error: "Publish error" });
     }
-
-    res.json({ message: "Data received" });
 };
